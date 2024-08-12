d9d1d0060d368d13373fbc6a9b7c9dbf652df14b
Pooriya Samadifar <pooriyasmd@gmail.com>
2024-08-12 10:12:31 +0200

    Patch

--- Mytest.m
+++ Mytest.m
@@ -1,12 +1,16 @@
-x = -10:0.01:10;
-plot(x,Rect(x+7),'LineWidth',1.5)
+% Calculate the impedance of the dipole created in example 1 over the
+% frequency span 50MHz - 100MHz.
 
-function y = Rect(x)
-
-    y=zeros(size(x)); 
-    ind=find(x>0 & x<1);
-    y(ind)=1;
-    ind=find(x==0 | x==1);
-    y(ind)=0.5; % to add up correctly at boundary
-
-end
+clc;clear;close all
+p = patchMicrostrip('Length',75e-3, 'Width', 37.5e-3,               ...
+        'GroundPlaneLength', 120e-3, 'GroundPlaneWidth', 120e-3,        ...
+        'FeedOffset', [-18.75e-3 0]);
+figure()
+impedance(p,linspace(1.70e9,1.75e9,51))
+figure()
+pattern(p, 1.75e9)
+figure()
+current(p, 1.75e9, "metal")
+figure()
+beamwidth(p, 1.75e9, 1:1:360, 140)
+show(p)
